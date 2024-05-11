ca86231ea4db73bba6ad272349d3e262dab3205c
hollycosslett <154456339+hollycosslett@users.noreply.github.com>
2024-05-11 12:35:47 +0100

    Changed html and added styling with CSS

--- src/Weather.js
+++ src/Weather.js
@@ -6,33 +6,55 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <input
-          type="search"
-          placeholder="Enter a city..."
-          className="form-search-bar"
-        />
-        <input type="submit" value="submit" className="btn btn-primary" />
-      </form>
-      <h1>London</h1>
-      <ul>
-        <li>Saturday 09.58</li>
-        <li>Cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="cloudy icon"
-          />
-          14°C
+        <div className="row">
+          <div className="col-10 p-1">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-search-bar"
+            />
+          </div>
+          <div className="col-2 p-1">
+            <input type="submit" value="Submit" className="btn search-button" />
+          </div>
         </div>
-        <div className="col-6">
+      </form>
+      <div className="row weather-overview">
+        <div className="col-md-6">
+          <h1>London</h1>
           <ul>
-            <li>Precipitation: 1%</li>
-            <li>Humidity: 85%</li>
-            <li>Wind: 5mph</li>
+            <li>Saturday 09.58</li>
+            <li>Cloudy</li>
           </ul>
         </div>
+        <div className="col-md-6">
+          <div className="weather-overview-right">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="cloudy icon"
+              className="main-icon img-fluid"
+            />
+            <div className="temperature">
+              14<span className="units">°C</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="additional-details">
+        <ul>
+          <li>
+            Precipitation:{" "}
+            <div className="additional-details-measurements d-inline">1%</div>
+          </li>
+          <li>
+            Humidity:{" "}
+            <div className="additional-details-measurements d-inline">80%</div>
+          </li>
+          <li>
+            Wind:{" "}
+            <div className="additional-details-measurements d-inline">5mph</div>
+          </li>
+        </ul>
       </div>
     </div>
   );
