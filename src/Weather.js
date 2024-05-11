02e6f1fca6c3ff590e82c1d6db5a3bb419675085
hollycosslett <154456339+hollycosslett@users.noreply.github.com>
2024-05-11 10:12:36 +0100

    Created html structure for Weather component

--- src/Weather.js
+++ src/Weather.js
@@ -3,5 +3,37 @@ import App from "./App";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello from Weather</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          className="form-search-bar"
+        />
+        <input type="submit" value="submit" className="btn btn-primary" />
+      </form>
+      <h1>London</h1>
+      <ul>
+        <li>Saturday 09.58</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+            alt="cloudy icon"
+          />
+          14°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 1%</li>
+            <li>Humidity: 85%</li>
+            <li>Wind: 5mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
