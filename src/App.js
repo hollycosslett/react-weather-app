dc9974dd0dd166c37cbb2368b02ca97ebb827053
hollycosslett <154456339+hollycosslett@users.noreply.github.com>
2024-05-11 09:41:15 +0100

    imported bootstrap and checked it was working

--- src/App.js
+++ src/App.js
@@ -4,31 +4,33 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <p>
-          {" "}
-          This project was coded by{" "}
-          <a href="https://github.com/hollycosslett" target="_blank">
-            Holly Cosslett
-          </a>
-          ,{" "}
-          <a
-            href="https://github.com/hollycosslett/react-weather-app"
-            target="_blank"
-          >
-            open-sourced{" "}
-          </a>
-          on GitHub and hosted on{" "}
-          <a
-            href="https://shecodes-react-weather-appplication.netlify.app/"
-            target="_blank"
-          >
-            Netlify
-          </a>
-          .
-        </p>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <p>
+            {" "}
+            This project was coded by{" "}
+            <a href="https://github.com/hollycosslett" target="_blank">
+              Holly Cosslett
+            </a>
+            ,{" "}
+            <a
+              href="https://github.com/hollycosslett/react-weather-app"
+              target="_blank"
+            >
+              open-sourced{" "}
+            </a>
+            on GitHub and hosted on{" "}
+            <a
+              href="https://shecodes-react-weather-appplication.netlify.app/"
+              target="_blank"
+            >
+              Netlify
+            </a>
+            .
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
