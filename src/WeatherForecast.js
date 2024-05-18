ec25f877124c6750f324d4d9a5baaf96c1ce08d9
hollycosslett <154456339+hollycosslett@users.noreply.github.com>
2024-05-18 17:23:56 +0100

    used useEffect to display forecast temperatures and changed styling

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,6 +6,11 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setloaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setloaded(false);
+  }, [props.coordinates]);
+
   function getForecast(response) {
     setForecast(response.data.daily);
     setloaded(true);
@@ -21,8 +26,9 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay forecastData={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
-            return null;
           })}
         </div>
       </div>
@@ -35,4 +41,5 @@ export default function WeatherForecast(props) {
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(getForecast);
   }
+  return null;
 }
